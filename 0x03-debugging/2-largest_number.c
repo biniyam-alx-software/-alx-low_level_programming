1d2cdf55a2ad8c3352e31c2fb57c77b05b756b3c
root <bini>
2022-04-15 16:35:52 -0700

    fourth commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,16 +12,25 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && b > c)
+    if (a > b )
     {
-        largest = a;
+
+        if (a > c)
+	{
+
+	  largest = a;
+
+	}
+	else{
+	  largest = c;
+	}
+
     }
-    else if (b > a && a > c)
+    else if(b > c)
     {
         largest = b;
     }
-    else if(c > a && c > b)
-    {
+    else{
         largest = c;
     }
 
