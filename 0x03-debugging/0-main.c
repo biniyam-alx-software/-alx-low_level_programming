a3491eee608b0c055eb4cb45bf4a288cab71c912
root <bini>
2022-04-15 02:36:39 -0700

    some change

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,10 +6,19 @@
 
 int main(void)
 {
-int i;
+  int i;
 
-i = 0;
-positive_or_negative(i);
+  i = 0;
+ 
+  positive_or_negative(i);
 
-return (0);
+  return (0);
+}
+void positive_or_negative(int n){
+        if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);  
 }
