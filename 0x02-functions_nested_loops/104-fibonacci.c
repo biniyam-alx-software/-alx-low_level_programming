0de2d3af133b6a3dd987fa7b19eb01c6d5e58dca
root <bini>
2022-04-15 01:20:17 -0700

    some change advanced

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+
+  int i, n;
+
+  int t1 = 1, t2 = 2;
+
+  int nextTerm = t1 + t2;
+
+  printf("%d, %d, ", t1, t2);
+
+  for (i = 3; i <= n; ++i) {
+    if (i != n)
+      printf("%d, ", nextTerm);
+    else
+      printf("%d", nextTerm);
+    t1 = t2;
+    t2 = nextTerm;
+    nextTerm = t1 + t2;
+  }
+  printf("\n");
+
+  return 0;
+}
